8619efbfa98a8665497d57f0e467e239a2718011
starc52 <cstarpowful@gmail.com>
2022-01-04 21:41:33 +0530

    ada please work

--- preprocess.py
+++ preprocess.py
@@ -26,175 +26,175 @@ import warnings
 warnings.filterwarnings('ignore')
 
 # class L2Norm(nn.Module):
-# 	def __init__(self):
-# 		super(L2Norm, self).__init__()
+#     def __init__(self):
+#         super(L2Norm, self).__init__()
 
-# 	def forward(self, x):
-# 		x = x / torch.norm(x, dim=1, keepdim=True)
-# 		return x
+#     def forward(self, x):
+#         x = x / torch.norm(x, dim=1, keepdim=True)
+#         return x
 
 # class VGGFace2Model(nn.Module):
-# 	def __init__(self, 
-# 				embedding_dim=4096,
-# 				arch_type='resnet'
-# 			):
-# 		super(VGGFace2Model, self).__init__()
-# 		print(arch_type)
-# 		if arch_type=='resnet':
-# 			self.backbone = resnet50('/home/starc52/audret/resnet.pth')
-# 		else:
-# 			self.backbone = senet50('/home/starc52/audret/senet.pth')
-		
-# 		print("Weights Loaded!")
+#     def __init__(self, 
+#                 embedding_dim=4096,
+#                 arch_type='resnet'
+#             ):
+#         super(VGGFace2Model, self).__init__()
+#         print(arch_type)
+#         if arch_type=='resnet':
+#             self.backbone = resnet50('/home/starc52/audret/resnet.pth')
+#         else:
+#             self.backbone = senet50('/home/starc52/audret/senet.pth')
+        
+#         print("Weights Loaded!")
 
-# 		for param in self.backbone.parameters():
-# 			# param.requires_grad = True
-# 			param.requires_grad = False
+#         for param in self.backbone.parameters():
+#             # param.requires_grad = True
+#             param.requires_grad = False
 
-# 	def forward(self, x):
-# 		batch_size = x.size(0)
-# 		x = self.backbone(x)[1]
-# 		x = x.view(batch_size, -1)
-# 		# x = self.fc(x)
-		
-# 		return x
+#     def forward(self, x):
+#         batch_size = x.size(0)
+#         x = self.backbone(x)[1]
+#         x = x.view(batch_size, -1)
+#         # x = self.fc(x)
+        
+#         return x
 
 
 # class FaceFeatureExtractor(nn.Module):
-# 	def __init__(self, 
-# 				embedding_dim=256
-# 			):
-# 		super(FaceFeatureExtractor, self).__init__()
+#     def __init__(self, 
+#                 embedding_dim=256
+#             ):
+#         super(FaceFeatureExtractor, self).__init__()
 
-# 		self.backbone = InceptionResnetV1(pretrained='vggface2')
+#         self.backbone = InceptionResnetV1(pretrained='vggface2')
 
-# 		for param in self.backbone.parameters():
-# 			param.requires_grad = False
+#         for param in self.backbone.parameters():
+#             param.requires_grad = False
 
-# 		# self.fc = nn.Sequential(
-# 		# 	nn.Linear(512, embedding_dim),
-# 		# 	nn.ReLU(inplace=True)
-# 		# )
-# 		# self.activation = nn.Sigmoid()
+#         # self.fc = nn.Sequential(
+#         #     nn.Linear(512, embedding_dim),
+#         #     nn.ReLU(inplace=True)
+#         # )
+#         # self.activation = nn.Sigmoid()
 
-# 	def forward(self, x):
-# 		x = self.backbone(x)
-# 		# x = self.fc(x)
-# 		# x = x / torch.norm(x, dim=1, keepdim=True)
-# 		# # x = self.activation(x)
-		
-# 		return x
+#     def forward(self, x):
+#         x = self.backbone(x)
+#         # x = self.fc(x)
+#         # x = x / torch.norm(x, dim=1, keepdim=True)
+#         # # x = self.activation(x)
+        
+#         return x
 
 # class ImageInference():
-# 	def __init__(self, model_val_path, arch_type="senet"):
-# 		self.model_val_path = model_val_path
-# 		self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# 		print(self.device)
+#     def __init__(self, model_val_path, arch_type="senet"):
+#         self.model_val_path = model_val_path
+#         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#         print(self.device)
 
-# 		if arch_type=="facenet":
-# 			self.model = FaceFeatureExtractor()
-# 			self.model.load_state_dict(torch.load(model_val_path))
-# 		else:
-# 			self.model = VGGFace2Model(arch_type=arch_type)
+#         if arch_type=="facenet":
+#             self.model = FaceFeatureExtractor()
+#             self.model.load_state_dict(torch.load(model_val_path))
+#         else:
+#             self.model = VGGFace2Model(arch_type=arch_type)
 
-# 		print("Model weights loaded from", model_val_path)
-# 		self.model.to(self.device)
-# 		self.model.eval()
+#         print("Model weights loaded from", model_val_path)
+#         self.model.to(self.device)
+#         self.model.eval()
 
-# 		self.img_transform = ImageTransform(arch_type=arch_type)
-		
+#         self.img_transform = ImageTransform(arch_type=arch_type)
+        
 
-# 	def get_signature(self, img_path, save_path=None, img=None):
-# 		# img = self.img_transform.transform(img_path, img=img)
-# 		with torch.no_grad():
-# 			dev_img = img.unsqueeze(0)
-# 			batch_img = dev_img.to(self.device)
-# 			embedding = self.model(batch_img)
+#     def get_signature(self, img_path, save_path=None, img=None):
+#         # img = self.img_transform.transform(img_path, img=img)
+#         with torch.no_grad():
+#             dev_img = img.unsqueeze(0)
+#             batch_img = dev_img.to(self.device)
+#             embedding = self.model(batch_img)
 
-# 		embedding = embedding.cpu().numpy()
-# 		if save_path is not None:
-# 			np.save(save_path, embedding)
-# 			return
-# 		return embedding
+#         embedding = embedding.cpu().numpy()
+#         if save_path is not None:
+#             np.save(save_path, embedding)
+#             return
+#         return embedding
 
 class AudioInference():
-	def __init__(self):
-		self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-		print(self.device)
+    def __init__(self):
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        print(self.device)
 
-		# self.model=VGGM(1251)
-		# # self.model.load_state_dict(torch.load("../VGGVox-PyTorch-master/models/VGGM300_BEST_140_81.99.pth", map_location=device))
-		# self.model.load_state_dict(torch.load("/home/starc52/audret/models/VGGM300_BEST_140_81.99.pth", map_location=self.device))
-		# self.model.to(self.device)
-		# print("VggVox model loaded")
-		# self.model.eval()
-		# self.preprocess = preprocess(arch_type=arch_type)
-		
+        # self.model=VGGM(1251)
+        # # self.model.load_state_dict(torch.load("../VGGVox-PyTorch-master/models/VGGM300_BEST_140_81.99.pth", map_location=device))
+        # self.model.load_state_dict(torch.load("/home/starc52/audret/models/VGGM300_BEST_140_81.99.pth", map_location=self.device))
+        # self.model.to(self.device)
+        # print("VggVox model loaded")
+        # self.model.eval()
+        # self.preprocess = preprocess(arch_type=arch_type)
+        
 
-	def get_audio_features(self, audio_path, save_path=None, img=None):
+    def get_audio_features(self, audio_path, save_path=None, img=None):
 
-		audio,sr=librosa.load(audio_path,sr=16000)
+        audio,sr=librosa.load(audio_path,sr=16000)
 
-		audio = audio[0:48000]
+        audio = audio[0:48000]
 
-		audio = preprocess(audio).astype(np.float32)
-		audio=np.expand_dims(audio, 2)
+        audio = preprocess(audio).astype(np.float32)
+        audio=np.expand_dims(audio, 2)
 
-		transformers=transforms.ToTensor()
-		audio = transformers(audio)
-		audio = audio.unsqueeze(0)
+        transformers=transforms.ToTensor()
+        audio = transformers(audio)
+        audio = audio.unsqueeze(0)
 
-		audio = audio.to(self.device)
+        audio = audio.to(self.device)
 
-		with torch.no_grad():
-			audio_embedding = self.model(audio)
+        with torch.no_grad():
+            audio_embedding = self.model(audio)
 
-		audio_embedding = audio_embedding.cpu().numpy()
-	
-		return audio_embedding
+        audio_embedding = audio_embedding.cpu().numpy()
+    
+        return audio_embedding
 
-	# todo
-	#takes audio as input, splits it with window and stride params, then comptues the embeddings
-	def split_audio(self, audio, window=3, stride=2, sr=16000) :
-		# window and stride is in seconds
+    # todo
+    #takes audio as input, splits it with window and stride params, then comptues the embeddings
+    def split_audio(self, audio, window=3, stride=2, sr=16000) :
+        # window and stride is in seconds
 
-		if type(audio)==str:
-			pathToAudio = audio
-			audio,sr=librosa.load(audio,sr=16000)
+        if type(audio)==str:
+            pathToAudio = audio
+            audio,sr=librosa.load(audio,sr=16000)
 
-		nsamples_per_window = int(window*sr)
-		samples_per_stride = int(stride*sr)
+        nsamples_per_window = int(window*sr)
+        samples_per_stride = int(stride*sr)
 
-		slice_start = [i for i in range(0,len(audio)-nsamples_per_window,samples_per_stride)]
-		slice_end = [i+nsamples_per_window if i+nsamples_per_window<len(audio) else len(audio) for i in slice_start]
-		
-		audios = [audio[slice_start[i]:slice_end[i]] for i in range(len(slice_start))]
-		audios = random.sample(audios, 1)
-		audios_processed = [preprocess(audio).astype(np.float32) for audio in audios]
-		audios_expanded = [np.expand_dims(audio, 2) for audio in audios_processed]
-		try:
-			os.makedirs(pathToAudio[:-4])
-			[np.save(join(pathToAudio[:-4], "%03d.npy"%idx), audio_fft) for idx, audio_fft in enumerate(audios_expanded)]
-			return audios_expanded
-		except:
-			print("audio_fft directory already present")
-		# transformers = transforms.ToTensor()
-		# audios_transformed = [transformers(audio) for audio in audios_expanded]
-		# audios_unsqzd = [audio.unsqueeze(0) for audio in audios_transformed]
-		# embeddings = []
-		# try:
-		# 	os.makedirs(pathToAudio[:-4])
-		# 	for idx, audio in enumerate(audios_unsqzd):
-		# 		audio_temp = audio.to(self.device)
-		# 		with torch.no_grad():
-		# 			audio_embedding = self.model(audio_temp)
-		# 		audio_embedding = audio_embedding.cpu().numpy()
-		# 		np.save(join(pathToAudio[:-4], "%03d.npy"%idx), audio_embedding)
-		# 		embeddings.append(audio_embedding)
-		# except:
-		# 	print("directory present")
-		
-		# return embeddings
+        slice_start = [i for i in range(0,len(audio)-nsamples_per_window,samples_per_stride)]
+        slice_end = [i+nsamples_per_window if i+nsamples_per_window<len(audio) else len(audio) for i in slice_start]
+        
+        audios = [audio[slice_start[i]:slice_end[i]] for i in range(len(slice_start))]
+        audios = random.sample(audios, 1)
+        audios_processed = [preprocess(audio).astype(np.float32) for audio in audios]
+        audios_expanded = [np.expand_dims(audio, 2) for audio in audios_processed]
+        try:
+            os.makedirs(pathToAudio[:-4])
+            [np.save(join(pathToAudio[:-4], "%03d.npy"%idx), audio_fft) for idx, audio_fft in enumerate(audios_expanded)]
+            return audios_expanded
+        except:
+            print("audio_fft directory already present")
+        # transformers = transforms.ToTensor()
+        # audios_transformed = [transformers(audio) for audio in audios_expanded]
+        # audios_unsqzd = [audio.unsqueeze(0) for audio in audios_transformed]
+        # embeddings = []
+        # try:
+        #     os.makedirs(pathToAudio[:-4])
+        #     for idx, audio in enumerate(audios_unsqzd):
+        #         audio_temp = audio.to(self.device)
+        #         with torch.no_grad():
+        #             audio_embedding = self.model(audio_temp)
+        #         audio_embedding = audio_embedding.cpu().numpy()
+        #         np.save(join(pathToAudio[:-4], "%03d.npy"%idx), audio_embedding)
+        #         embeddings.append(audio_embedding)
+        # except:
+        #     print("directory present")
+        
+        # return embeddings
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--root',default='/scratch/starc52/VoxCeleb2/dev/mp4', type=str)
@@ -216,45 +216,46 @@ mse_loss = AverageMeter()
 cosine_loss = AverageMeter()
 listOfIdentities=sorted(os.listdir(args.root))[args.start_id:args.end_id]
 for speaker_id in tqdm(sorted(os.listdir(args.root))[args.start_id:args.end_id]):#just 1000 user ids. 
-	for url in sorted(os.listdir(join(args.root, speaker_id))):
-		# os.makedirs(join(args.save_path, speaker_id, url), exist_ok=True)
-		for file_name in sorted(os.listdir(join(args.root, speaker_id, url))):
-			try:
-				if file_name[-4:] == ".mp4":
-					cap = cv2.VideoCapture(join(args.root, speaker_id, url, file_name))
-					transformToTensor = transforms.ToTensor()					
-					ret, img_file=cap.read()
-					faces = mtcnn(img_file)
-					if faces is None:
-						count=0
-						while cap.isOpened():
-							ret, frame = cap.read()
-					
-							img_file=np.array(frame, copy=True)
-							if not ret:
-								break
-							faces = mtcnn(img_file)
-							if ret and faces is not None:
-								cap.release()
-								break
-							count+=1
-							cap.set(1, count)
-					if faces is None:
-						print("No face detected for : %s" % join(args.root, speaker_id, url, file_name))
-						os.remove(join(args.root, speaker_id, url, file_name))
-						continue
-					img_file = faces[0]
-					img_file = img_file.permute(1, 2, 0).numpy()
-					img_file  = cv2.resize(img_file, (224, 224))
-					cv2.imwrite(join(args.root, speaker_id, url, file_name[:-4]+".jpg"), img_file)
-					os.remove(join(args.root, speaker_id, url, file_name))
-					print("Processed filename: %s"% join(args.root, speaker_id, url, file_name))
+    for url in sorted(os.listdir(join(args.root, speaker_id))):
+        # os.makedirs(join(args.save_path, speaker_id, url), exist_ok=True)
+#         for file_name in sorted(os.listdir(join(args.root, speaker_id, url))):
+        for file_name in sorted(os.listdir(join(args.root, speaker_id, url, "frames"))):
+            try:
+                if file_name[-4:] == ".":
+                    cap = cv2.VideoCapture(join(args.root, speaker_id, url, file_name))
+                    transformToTensor = transforms.ToTensor()                    
+                    ret, img_file=cap.read()
+                    faces = mtcnn(img_file)
+                    if faces is None:
+                        count=0
+                        while cap.isOpened():
+                            ret, frame = cap.read()
+                    
+                            img_file=np.array(frame, copy=True)
+                            if not ret:
+                                break
+                            faces = mtcnn(img_file)
+                            if ret and faces is not None:
+                                cap.release()
+                                break
+                            count+=1
+                            cap.set(1, count)
+                    if faces is None:
+                        print("No face detected for : %s" % join(args.root, speaker_id, url, file_name))
+                        os.remove(join(args.root, speaker_id, url, file_name))
+                        continue
+                    img_file = faces[0]
+                    img_file = img_file.permute(1, 2, 0).numpy()
+                    img_file  = cv2.resize(img_file, (224, 224))
+                    cv2.imwrite(join(args.root, speaker_id, url, file_name[:-4]+".jpg"), img_file)
+                    os.remove(join(args.root, speaker_id, url, file_name))
+                    print("Processed filename: %s"% join(args.root, speaker_id, url, file_name))
 
-				elif file_name[-4:] == ".wav":
-					utteranceEmbedding = AudioModel.split_audio(join(args.root, speaker_id, url, file_name))
-					os.remove(join(args.root, speaker_id, url, file_name))
-					print("Processed filename: %s"% join(args.root, speaker_id, url, file_name))
-			except:
-				print(traceback.format_exc())
-				continue			
+                elif file_name[-4:] == ".wav":
+                    utteranceEmbedding = AudioModel.split_audio(join(args.root, speaker_id, url, file_name))
+                    os.remove(join(args.root, speaker_id, url, file_name))
+                    print("Processed filename: %s"% join(args.root, speaker_id, url, file_name))
+            except:
+                print(traceback.format_exc())
+                continue            
 print(listOfIdentities)
